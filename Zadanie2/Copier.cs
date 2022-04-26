9aad1ceeb1d534b9b3a56c00e7b9241b11dac91e
Adrian Włodarczyk <33695278+Shade644@users.noreply.github.com>
2022-04-26 21:07:28 +0200

    zadanie 2
    chyba dobrze ale już nie wiem

---
+++ Zadanie2/Copier.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ver1;
+
+namespace Zadanie1
+{
+    public class Copier : BaseDevice, IPrinter, IScanner
+    {
+        public int PrintCounter { get; set; }
+        public int ScanCounter { get; set; }
+        
+
+        public void Print(in IDocument doc)
+        {
+            if(GetState() == IDevice.State.on)
+            {
+                Console.WriteLine($"{DateTime.Now:dd.MM.yyyy HH:mm:ss} Print: {doc.GetFileName()}");
+                PrintCounter++;
+            }
+        }
+
+        public void Scan(out IDocument doc, IDocument.FormatType formatType = IDocument.FormatType.PDF)
+        {
+            doc = null;
+
+            if (GetState() == IDevice.State.on)
+            {
+                switch (formatType)
+                {
+                    case IDocument.FormatType.PDF:
+                        doc = new PDFDocument($"PDFScan{ScanCounter}.pdf");
+                        break;
+
+                    case IDocument.FormatType.JPG:
+                        doc = new ImageDocument($"ImageScan{ScanCounter}.jpg");
+                        break;
+
+                    case IDocument.FormatType.TXT:
+                        doc = new TextDocument($"TextScan{ScanCounter}.txt");
+                        break;
+                }
+
+                Console.WriteLine($"{DateTime.Now:dd.MM.yyyy HH:mm:ss} Scan: {doc.GetFileName()}");
+                ScanCounter++;
+            }
+        }
+
+        public void ScanAndPrint()
+        {
+            if (GetState() == IDevice.State.on)
+            {
+                Scan(out IDocument doc);
+                Print(in doc);
+            }
+        }
+    }
+}
---
+++ Zadanie2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ver1;
+using Zadanie2;
+
+namespace zadanie
+{
+    class Program
+    {
+        static void Main()
+        {
+            var xerox = new MultiFunctionalDevice();
+            xerox.PowerOn();
+            IDocument doc1 = new PDFDocument("aaa.pdf");
+            xerox.Print(in doc1);
+
+            IDocument doc2;
+            xerox.Scan(out doc2);
+
+            xerox.ScanAndPrint();
+            System.Console.WriteLine(xerox.Counter);
+            System.Console.WriteLine(xerox.PrintCounter);
+            System.Console.WriteLine(xerox.ScanCounter);
+
+            xerox.ScanPrintFax();
+            System.Console.WriteLine(xerox.Counter);
+            System.Console.WriteLine(xerox.ScanCounter);
+            System.Console.WriteLine(xerox.PrintCounter);
+            
+
+        }
+    }
+}
